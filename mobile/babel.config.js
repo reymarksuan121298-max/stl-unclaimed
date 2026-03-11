bfc05e085f483bcabe5063a1003ca069c70001d6
github.com/reymarksuan121298-max <reymarksuan121298@gmail.com>
2026-03-11 14:03:02 +0800

    feat: Initialize new Expo React Native mobile application with essential configurations and dependencies.

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,16 +1,9 @@
 module.exports = function (api) {
-    api.cache(true)
-    return {
-        presets: ['babel-preset-expo'],
-        plugins: [
-            '@babel/plugin-proposal-optional-chaining',
-            '@babel/plugin-proposal-nullish-coalescing-operator',
-            '@babel/plugin-proposal-logical-assignment-operators',
-            '@babel/plugin-transform-template-literals',
-            '@babel/plugin-transform-arrow-functions',
-            '@babel/plugin-transform-shorthand-properties',
-            '@babel/plugin-transform-spread',
-            'react-native-reanimated/plugin',
-        ],
-    }
-}
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      'react-native-reanimated/plugin',
+    ],
+  };
+};
